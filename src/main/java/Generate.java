e3fd05078030fcdd0e5a1dd2cb6039e44e11eeb4
irmazhinkina <irma.zhinkina@jetbrains.com>
2023-10-10 10:16:05 +0000

    add genarate

---
+++ src/main/java/Generate.java
@@ -0,0 +1,12 @@
+public class Generate {
+	int xxx = 5;
+
+	public Generate() {
+		this.xxx = xxx;
+	}
+
+	public static void main(String[] args) {
+		Generate myObj = new Generate();
+		System.out.println(myObj.xxx);
+	}
+}
