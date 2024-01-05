073feaee85a6eb3dfef1fdbcf408690728d2f6aa
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-01-05 16:55:37 +0000

    Add new Test class in petclinic package
    A new class, named Test, has been added to the org.springframework.samples.petclinic package. This class contains a static main method accepting string array (args) and integer (i) for printing the value of i on the console.

---
+++ src/main/java/org/springframework/samples/petclinic/Test.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic;
+
+import java.io.PrintStream;
+
+public class Test {
+	private static PrintStream out;
+
+	public static void main(String[] args, int i) {
+		out.println(i);
+	}
+}
