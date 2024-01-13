4253fb05a76d512e9d30f0707b972b7e0cac597a
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-01-13 08:58:03 +0000

    Revert "Revert "Add Generate files""
    This reverts commit 4e5759a3b746ee289db7ba6bba86043192f225c0.

---
+++ src/test/java/GenerateTest.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GenerateTest extends TestCase {
+
+	@Test
+	void main() {
+	}
+}
