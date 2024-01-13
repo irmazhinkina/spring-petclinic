a385f3b6acee535908d1682a2586691241f1da13
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-01-13 09:02:17 +0000

    Revert "Add Generate files"
    This reverts commit 19cf3ada405b254241c800539a3607d0f797829e.

---
+++ src/main/java/Generate.java
@@ -0,0 +1,14 @@
+public class Generate {
+	static int xxx = 5;
+
+
+
+	public static void main(String[] args) {
+		Generate myObj = new Generate();
+		System.out.println(xxx);
+	}
+
+	public Generate() {
+		this.xxx = xxx;
+	}
+}
