ed8fdfba70914ca8b0c76f98c6bc89833cd7a045
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-02-29 15:15:56 +0000

    123

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
-		int xxx = 123;
-		xxx = 2;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter something:");
+		String input = scanner.nextLine();
+
+		System.out.println("You entered: " + input);
+		//cvdv
 	}
-
-
 }
