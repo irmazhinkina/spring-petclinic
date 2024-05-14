679ccfd9786a64557118081866b4b3e2037eb9bd
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-05-14 12:27:56 +0000

    new file

--- src/main/java/Test123.java
+++ src/main/java/Test123.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class Test123 {
 	public static void main(String[] args) {
 		System.out.println();
-		System.out.printf("");
 		System.out.println("args = " + Arrays.toString(args));
 	}
 }
