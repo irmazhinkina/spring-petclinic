5d6ab94aece4cdc6189f31f502c2ea6f8880a47c
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-05-14 12:27:23 +0000

    new file

---
+++ src/main/java/Test123.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Test123 {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.printf("");
+		System.out.println("args = " + Arrays.toString(args));
+	}
+}
